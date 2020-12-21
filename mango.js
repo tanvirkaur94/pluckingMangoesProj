1154848259618a4b27d406e8911246feef7b2bf6
tanvirkaur94 <71765229+tanvirkaur94@users.noreply.github.com>
2020-12-21 12:25:14 +0530

    Update mango.js
    added r

--- mango.js
+++ mango.js
@@ -1,14 +1,15 @@
 class Mango{
-    constructor(x, y) {
+    constructor(x, y,r) {
         var options = {
             isStatic:true,
             restitution:0,
             friction:1.0,
             density:1.2
         }
-        this.body = Bodies.circle(x, y, 15, options);
+        this.body = Bodies.circle(x, y, r, options);
         this.x = x;
         this.y = y;
+        this.r= r;
         this.image = loadImage("mango.png");
         World.add(world, this.body);
     }
@@ -18,7 +19,7 @@ class Mango{
         translate(this.body.position.x, this.body.position.y);
         rotate(this.body.angle)
         imageMode(CENTER);
-        image(this.image, 0, 0, 60, 60);
+        image(this.image, 0, 0, this.r*2,this.r*2);
         pop();
     } 
 }
